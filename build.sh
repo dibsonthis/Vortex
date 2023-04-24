c4864df4fbff1fe9a6328273d4ba309cfbfc71eb
Adib Attie <adib@192-168-1-129.tpgi.com.au>
2023-04-24 20:10:23 +1000

    added build.sh

---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+macBuild()
+{
+    clang++ \
+    -O3 \
+    -std=c++20 \
+    -stdlib=libc++ \
+    "$PWD"/src/Node/Node.cpp \
+    "$PWD"/src/Lexer/Lexer.cpp \
+    "$PWD"/src/Parser/Parser.cpp \
+    "$PWD"/src/Interpreter/Interpreter.cpp \
+    "$PWD"/src/utils/utils.cpp \
+    "$PWD"/main.cpp \
+    -o "$PWD"/bin/build/interp/mac/vortex
+}
+
+winBuild()
+{
+    g++ \
+    -O3 \
+    -std=c++20 \
+    -stdlib=libc++ \
+    "$PWD"/src/Node/Node.cpp \
+    "$PWD"/src/Lexer/Lexer.cpp \
+    "$PWD"/src/Parser/Parser.cpp \
+    "$PWD"/src/Interpreter/Interpreter.cpp \
+    "$PWD"/src/utils/utils.cpp \
+    "$PWD"/main.cpp \
+    -o "$PWD"/bin/build/interp/windows/vortex.exe
+}
+
+linBuild()
+{
+    g++ \
+    -O3 \
+    -std=c++20 \
+    -stdlib=libc++ \
+    "$PWD"/src/Node/Node.cpp \
+    "$PWD"/src/Lexer/Lexer.cpp \
+    "$PWD"/src/Parser/Parser.cpp \
+    "$PWD"/src/Interpreter/Interpreter.cpp \
+    "$PWD"/src/utils/utils.cpp \
+    "$PWD"/main.cpp \
+    -o "$PWD"/bin/build/interp/windows/vortex.exe
+}
+
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    linBuild
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    winBuild
+    ;;
+  'Darwin') 
+    OS='Mac'
+    echo "Building on Mac..."
+    macBuild
+    ;;
+  *) ;;
+esac
