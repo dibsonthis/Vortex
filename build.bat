2ca936ebf3c7a896170b92f1aa221ea894872fae
Adib Attie <adib@192-168-1-129.tpgi.com.au>
2023-04-28 15:20:17 +1000

    added mkdirs

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+mkdir %CD%/bin/build/interp
+mkdir %CD%/bin/build/interp/linux
 g++ -O3 -std=c++20 %CD%/src/Node/Node.cpp %CD%/src/Lexer/Lexer.cpp %CD%/src/Parser/Parser.cpp %CD%/src/Interpreter/Interpreter.cpp %CD%/src/utils/utils.cpp %CD%/main.cpp -o %CD%/bin/build/interp/windows/vortex.exe
