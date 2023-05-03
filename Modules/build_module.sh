ff720d30077d5ec495bac02850c919284e2ad27a
Adib Attie <adib@Adib-Atties-Mac.local>
2023-05-03 17:11:39 +1000

    fixed pkg file

--- Modules/build_module.sh
+++ Modules/build_module.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+
+LIBS=""
+if [ -z "$(ls -A modules/$1/lib)" ]; then
+   LIBS=""
+else
+    LIBS="modules/$1/lib/lib*"
+fi
+
 clang++ \
 -dynamiclib \
 -O3 \
 -Wno-everything \
 -I modules/"$1"/include \
--L modules/"$1"/libraries \
+-L modules/"$1"/lib \
+$LIBS \
 -std=c++20 \
 -stdlib=libc++ \
 "$PWD"/modules/"$1"/"$1".cpp \
