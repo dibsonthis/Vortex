0670abff51f16279053802ef187baaa263c5830d
Adib Attie <adib@192-168-1-129.tpgi.com.au>
2023-05-04 16:42:07 +1000

    idk, maybe fix sdl lib

--- Modules/build_modules.sh
+++ Modules/build_modules.sh
@@ -20,6 +20,13 @@ fi
 
 CONFIG=""
 
+if [ "$FILE" = "sdl" ]; then
+    sudo install_name_tool -id "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" "$FILE/lib/libSDL2-2.0.0.dylib"
+    CONFIG="-L/usr/local/share/vortex/modules/lib -I/usr/local/share/vortex/modules/SDL2 -D_THREAD_SAFE"
+else
+    CONFIG=""
+fi
+
 clang++ \
 -dynamiclib \
 -O3 \
@@ -34,11 +41,11 @@ $LIBS \
 -o "$PWD"/$FILE/bin/$FILE \
 -Wl,-rpath "$PWD"/$FILE/lib
 
-if [ "$FILE" = "sdl" ]; then
-    sudo install_name_tool -id "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" "$FILE/lib/libSDL2-2.0.0.dylib"
-    sudo install_name_tool -change "$PWD"/$FILE/lib/libSDL2-2.0.0.dylib "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" $FILE/bin/sdl
-else
-    CONFIG=""
-fi
+# if [ "$FILE" = "sdl" ]; then
+#     sudo install_name_tool -id "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" "$FILE/lib/libSDL2-2.0.0.dylib"
+#     sudo install_name_tool -change "$PWD"/$FILE/lib/libSDL2-2.0.0.dylib "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" $FILE/bin/sdl
+# else
+#     CONFIG=""
+# fi
 
 done
