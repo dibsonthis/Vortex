b44af899997ba9905b181241cf74fdddb31d7b6b
Adib Attie <adib@192-168-1-129.tpgi.com.au>
2023-05-04 13:51:58 +1000

    fixed sdl bindings

--- Modules/build_modules.sh
+++ Modules/build_modules.sh
@@ -6,6 +6,10 @@ cd modules
 for FILE in *; 
 do
 
+mkdir $FILE/bin
+mkdir $FILE/lib
+mkdir $FILE/include
+
 echo Compiling $FILE
 
 if [ -z "$(ls -A $FILE/lib)" ]; then
@@ -14,9 +18,7 @@ else
     LIBS="$FILE/lib/lib*"
 fi
 
-mkdir $FILE/bin
-mkdir $FILE/lib
-mkdir $FILE/include
+CONFIG=""
 
 clang++ \
 -dynamiclib \
@@ -24,10 +26,19 @@ clang++ \
 -Wno-everything \
 -I$FILE/include \
 -L$FILE/lib \
+$CONFIG \
 $LIBS \
 -std=c++20 \
 -stdlib=libc++ \
 "$PWD"/$FILE/$FILE.cpp \
 -o "$PWD"/$FILE/bin/$FILE \
 -Wl,-rpath "$PWD"/$FILE/lib
+
+if [ "$FILE" = "sdl" ]; then
+    sudo install_name_tool -id "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" "$FILE/lib/libSDL2-2.0.0.dylib"
+    sudo install_name_tool -change "$PWD"/$FILE/lib/libSDL2-2.0.0.dylib "/usr/local/share/vortex/modules/sdl/lib/libSDL2-2.0.0.dylib" $FILE/bin/sdl
+else
+    CONFIG=""
+fi
+
 done
