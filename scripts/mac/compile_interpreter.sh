fb2780ec5d132f38861644ec692ae78952cdac3d
Adib Attie <adib@192-168-1-104.tpgi.com.au>
2023-11-21 16:40:03 +1100

    added workflow

--- scripts/mac/compile_interpreter.sh
+++ scripts/mac/compile_interpreter.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-rm -r ../../bin/build/interp/mac
-mkdir -p ../../bin/build/interp/mac/vortex
+rm -r bin/build/interp/mac
+mkdir -p bin/build/interp/mac/vortex
 
 clang++ \
 -Ofast \
 -Wno-everything \
 -std=c++20 \
-../../src/Node/Node.cpp \
-../../src/Lexer/Lexer.cpp \
-../../src/Parser/Parser.cpp \
-../../src/Interpreter/Interpreter.cpp \
-../../src/Typechecker/Typechecker.cpp \
-../../src/Bytecode/Bytecode.cpp \
-../../src/Bytecode/Generator.cpp \
-../../src/VirtualMachine/VirtualMachine.cpp \
-../../src/utils/utils.cpp \
-../../main.cpp \
--o ../../bin/build/interp/mac/vortex
+src/Node/Node.cpp \
+src/Lexer/Lexer.cpp \
+src/Parser/Parser.cpp \
+src/Interpreter/Interpreter.cpp \
+src/Typechecker/Typechecker.cpp \
+src/Bytecode/Bytecode.cpp \
+src/Bytecode/Generator.cpp \
+src/VirtualMachine/VirtualMachine.cpp \
+src/utils/utils.cpp \
+main.cpp \
+-o bin/build/interp/mac/vortex
