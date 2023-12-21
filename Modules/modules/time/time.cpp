a03701519641e40293d275be88a384f2a4e59906
Adib Attie <adib@192-168-1-104.tpgi.com.au>
2023-12-21 17:30:03 +1100

    added sleep

--- Modules/modules/time/time.cpp
+++ Modules/modules/time/time.cpp
@@ -3,12 +3,15 @@
 #include <ctime>
 #include <algorithm>
 #include <string>
+#include <thread>
 #include "include/Vortex.hpp"
 
-extern "C" Value datetime(std::vector<Value>& args) {
+extern "C" Value datetime(std::vector<Value> &args)
+{
     int num_required_args = 0;
 
-    if (args.size() != num_required_args) {
+    if (args.size() != num_required_args)
+    {
         error("Function 'datetime' expects " + std::to_string(num_required_args) + " argument(s)");
     }
 
@@ -19,4 +22,29 @@ extern "C" Value datetime(std::vector<Value>& args) {
     datetime.erase(std::remove(datetime.begin(), datetime.end(), '\n'), datetime.cend());
 
     return string_val(datetime);
+}
+
+extern "C" Value sleep(std::vector<Value> &args)
+{
+    using namespace std::chrono_literals;
+
+    int num_required_args = 1;
+
+    if (args.size() != num_required_args)
+    {
+        error("Function 'sleep' expects " + std::to_string(num_required_args) + " argument(s)");
+    }
+
+    Value ms = args[0];
+
+    if (!ms.is_number())
+    {
+        error("Function 'sleep' expects argument 'ms' to be a number");
+    }
+
+    auto _ms = ms.get_number() * 1ms;
+
+    std::this_thread::sleep_for(_ms);
+
+    return none_val();
 }
